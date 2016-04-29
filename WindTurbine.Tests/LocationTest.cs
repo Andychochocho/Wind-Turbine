97fbf3e36f82f7fe97907d3185934ddfad04eca1
Andrew Cho <cho-andrew@hotmail.com>
2016-04-29 15:01:57 -0700

    pass test for location model

---
+++ WindTurbine.Tests/LocationTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WindTurbine.Models;
+using Xunit;
+
+namespace WindTurbine.Tests
+{
+    public class LocationTest
+    {
+        [Fact]
+        public void GetNameTest()
+        {
+            //Arrange
+            var location = new Location();
+
+            //Act
+            location.Name = "Portland";
+            var result = location.Name;
+
+            //Assert
+            Assert.Equal("Portland", result);
+        }
+    }
+}
